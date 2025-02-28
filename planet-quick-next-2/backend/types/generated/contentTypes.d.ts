d0dfd15fc09f07e96a7322bf242898e0b1b27535
teranovaventures <nadir@teranovaventures.com>
2025-02-27 22:04:49 -0800

    Fixed username requirement, added auto-login after sign-up, fixed Forgot Password modal

--- planet-quick-next-2/backend/types/generated/contentTypes.d.ts
+++ planet-quick-next-2/backend/types/generated/contentTypes.d.ts
@@ -365,7 +365,6 @@ export interface AdminUser extends Struct.CollectionTypeSchema {
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
-    username: Schema.Attribute.String;
   };
 }
 
